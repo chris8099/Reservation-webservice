8ab05ed4ee4696df3ebed728c4f67c2cac1389af
chris8099 <a4565247@hotmil.com>
2022-01-10 15:37:20 +0800

    update

--- src/main/resources/schema-postgres.sql
+++ src/main/resources/schema-postgres.sql
@@ -1,18 +1,17 @@
 
-CREATE TABLE if exists not `reservation_table` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` varchar(100) DEFAULT NULL,
-  `Start_date` timestamp NULL DEFAULT NULL,
-  `end_date` timestamp NULL DEFAULT NULL,
-  `content` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+CREATE TABLE if not exists  reservation_table (
+  id SERIAL PRIMARY KEY,
+  user_id varchar(100) DEFAULT NULL,
+  Start_date timestamp NULL DEFAULT NULL,
+  end_date timestamp NULL DEFAULT NULL,
+  content varchar(100) DEFAULT NULL
+) ;
 
-CREATE TABLE if exists not `account_table` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` varchar(100) NOT NULL,
-  `user_pwd` varchar(100) DEFAULT NULL,
-  `user_name` varchar(100) DEFAULT NULL,
-  `user_email` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8
+
+CREATE TABLE if not exists  reservation_table (
+  id SERIAL PRIMARY KEY,
+  user_id varchar(100) DEFAULT NULL,
+  Start_date timestamp NULL DEFAULT NULL,
+  end_date timestamp NULL DEFAULT NULL,
+  content varchar(100) DEFAULT NULL
+) ;
