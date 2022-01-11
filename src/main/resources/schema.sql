0253862c9d7cc6d0869374a530b40e37889b7c85
chris8099 <a4565247@hotmil.com>
2022-01-11 22:08:16 +0800

    update

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,16 +1,16 @@
 
 CREATE TABLE if not exists account_table (
   id SERIAL PRIMARY KEY,
-  user_id varchar(100) NOT NULL,
-  user_pwd varchar(100) DEFAULT NULL,
-  user_name varchar(100) DEFAULT NULL,
-  user_email varchar(100) DEFAULT NULL
+  user_id text NOT NULL,
+  user_pwd text DEFAULT NULL,
+  user_name text DEFAULT NULL,
+  user_email text DEFAULT NULL
   );
 
 CREATE TABLE if not exists  reservation_table (
   id SERIAL PRIMARY KEY,
-  user_id varchar(100) DEFAULT NULL,
+  user_id text DEFAULT NULL,
   Start_date timestamp NULL DEFAULT NULL,
   end_date timestamp NULL DEFAULT NULL,
-  content varchar(100) DEFAULT NULL
+  content text DEFAULT NULL
 ) ;
